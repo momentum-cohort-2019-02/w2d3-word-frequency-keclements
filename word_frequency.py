4f2675c00624a81049386a235b9232b3da2c3bf6
keclements <kristin.clements@gmail.com>
2019-02-19 22:08:45 -0500

    commit missing

--- word_frequency.py
+++ word_frequency.py
@@ -4,12 +4,21 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+# 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     pass
 
+def normalize_text(text):
+    """
+    take imported text, lower case, remove punctuation--> remove whitespace-- 
+    """
+    text = text.lower()
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
 
+
+#imported text from ClintonÍ
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
@@ -25,3 +34,24 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
+
+ # remove punctuation
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+
+    text = new_text
+    text = text.replace("\n", " ")
+    return text
+
+# Print Word frequence from given file
+ print(
+# # get dictionary frequency    
+
+#print 
+
+
+
+
